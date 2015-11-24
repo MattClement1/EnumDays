882a74068c053b9379583fe541972ffcdd185ca1
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-11-24 12:42:41 -0500

    Enum Days

---
+++ EnumDays/EnumDays.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace EnumDays
+{
+    partial class frmEnumDays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lstDays = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(11, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(218, 16);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Click on the current day of the week:";
+            // 
+            // lstDays
+            // 
+            this.lstDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstDays.FormattingEnabled = true;
+            this.lstDays.ItemHeight = 16;
+            this.lstDays.Location = new System.Drawing.Point(60, 30);
+            this.lstDays.Name = "lstDays";
+            this.lstDays.Size = new System.Drawing.Size(120, 132);
+            this.lstDays.TabIndex = 1;
+            this.lstDays.SelectedIndexChanged += new System.EventHandler(this.lstDays_SelectedIndexChanged);
+            // 
+            // frmEnumDays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 194);
+            this.Controls.Add(this.lstDays);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "frmEnumDays";
+            this.Text = "Days of the Week";
+            this.Load += new System.EventHandler(this.frmEnumDays_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.ListBox lstDays;
+    }
+}
+
---
+++ EnumDays/EnumDays.cs
@@ -0,0 +1,58 @@
+﻿/*
+* Created by: Matthew Clement
+* Created on: 23-Nov-2015
+* Created for: Unit #6-01
+* This program uses an enum type to return the selected value
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumDays
+{
+    public partial class frmEnumDays : Form
+    {
+        enum DAYS
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        public frmEnumDays()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEnumDays_Load(object sender, EventArgs e)
+        {
+
+            foreach (DAYS singlePlanet in Enum.GetValues(typeof(DAYS)))
+            {
+                this.lstDays.Items.Add(singlePlanet);
+            }
+
+        }
+
+        private void lstDays_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedValue;
+            DAYS selectedDay;
+
+            selectedValue = Convert.ToString(this.lstDays.SelectedItem);
+            selectedDay = (DAYS)Enum.Parse(typeof(DAYS), selectedValue, true);
+            MessageBox.Show("The current day is: " + selectedDay + ".");
+        }
+    }
+}
